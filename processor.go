c4910123d7a19e152078770004082a4222fa9404
aleksadvaisly <aleks.advaisly@gmail.com>
2025-10-13 16:41:38 +0200

    feat: add Unicode checkbox support
    Convert Markdown checkboxes to Unicode square symbols:
    - [ ] → □ (U+25A1 WHITE SQUARE)
    - [x] → ■ (U+25A0 BLACK SQUARE)
    - [X] → ■ (U+25A0 BLACK SQUARE)
    
    Works with all embedded Unicode fonts (EB Garamond, DejaVu, Noto, Roboto, Merriweather, Source Serif).

--- processor.go
+++ processor.go
@@ -52,6 +52,9 @@ func (r *PdfRenderer) processText(node *ast.Text) {
 	if !r.NeedBlockquoteStyleUpdate {
 		s = strings.ReplaceAll(s, "\n", " ")
 	}
+	s = strings.ReplaceAll(s, "[ ]", "□")
+	s = strings.ReplaceAll(s, "[x]", "■")
+	s = strings.ReplaceAll(s, "[X]", "■")
 	r.tracer("Text", s)
 
 	if incell {
